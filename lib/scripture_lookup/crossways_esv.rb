9305746a798c66c1ed06e806460e24b0419c9d0a
Warren Wright <warren@thewrightangle.com>
2012-09-27 07:32:21 -0600

    Work in progress - added null objects

--- lib/scripture_lookup/crossways_esv.rb
+++ lib/scripture_lookup/crossways_esv.rb
@@ -1,9 +1,13 @@
 class CrosswaysEsv
 
-  # Examples
-  #  curl -G -d "key=IP&passage=John+1" http://www.esvapi.org/v2/rest/passageQuery
-  #  curl -G -d "key=IP&passage=John+3.16&output-format=plain-text&include-passage-references=false&include-footnotes=false&include-passage-horizontal-lines=false&include-heading-horizontal-lines=false&include-headings=false&include-verse-numbers=false&include-short-copyright=false" http://www.esvapi.org/v2/rest/passageQuery 
+  def initialize
+    @scripture_provider = NullProvider.new
+  end
+
+  attr_accessor :scripture_provider
+
   # TODO refactor book/chapter/verse into a class to encapsulate & decouple
   def lookup book, chapter, verse
+    @scripture_provider.lookup book, chapter, verse
   end
 end
--- spec/crossways_esv_spec.rb
+++ spec/crossways_esv_spec.rb
@@ -6,17 +6,32 @@ describe CrosswaysEsv do
     @crossways_esv = CrosswaysEsv.new
   end
 
-  describe "#new" do
-    it "returns a Crossways ESV strategy" do
-      @crossways_esv.should be_an_instance_of CrosswaysEsv
+  describe "without specifying a scripture provider" do
+    describe "#new" do
+      it "returns a Crossways ESV strategy with a NullProvider" do
+        @crossways_esv.should be_an_instance_of CrosswaysEsv
+        @crossways_esv.scripture_provider.should be_an_instance_of NullProvider
+      end
     end
   end
 
-  describe "#lookup" do
-    it "takes book, chapter, verse & returns scripture for John 3:16" do
-      @crossways_esv.lookup('John', 3, 16).should eql
-        "For God so loved the world, that he gave his only Son, that whoever "\
-        "believes in him should not perish but has eternal life."
+  describe "with the crossways EVS scripture provider" do
+    before :each do
+      @crossways_esv.scripture_provider = CrosswaysEsvProvider.new
+    end
+    describe "#new" do
+      it "returns a Crossways ESV strategy with a CrosswaysEsvProvider" do
+        @crossways_esv.should be_an_instance_of CrosswaysEsv
+        @crossways_esv.scripture_provider.should be_an_instance_of CrosswaysEsvProvider
+      end
+    end
+
+    describe "#lookup" do
+      it "takes book, chapter, verse & returns scripture for John 3:16" do
+        @crossways_esv.lookup('John', 3, 16).should eql
+          "For God so loved the world, that he gave his only Son, that whoever "\
+          "believes in him should not perish but has eternal life."
+      end
     end
   end
 end
