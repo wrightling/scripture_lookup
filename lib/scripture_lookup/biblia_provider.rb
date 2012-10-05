eee3cccf5959c25a4a4c683278c6750122de385d
Warren Wright <warren@thewrightangle.com>
2012-10-05 08:16:16 -0600

    Commented out BibliaProvider and tests - putting on hold

--- lib/scripture_lookup/biblia_provider.rb
+++ lib/scripture_lookup/biblia_provider.rb
@@ -1,16 +1,16 @@
-require 'httparty'
+#require 'httparty'
 
-module ScriptureLookup
-  class BibliaProvider
+#module ScriptureLookup
+  #class BibliaProvider
 
+    ###
+    ## Takes both a reference and a translation; returns a plain text translation.
     ##
-    # Takes both a reference and a translation; returns a plain text translation.
-    #
-    # Example:
-    #  curl -X GET -d "passage=John3.16&key=c96a9a1e68c80d8d14d990ffada473da" http://api.biblia.com/v1/bible/content/LEB.js 
-    def lookup reference, translation
-      puts "http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da"
-      HTTParty::get("http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da")
-    end
-  end
-end
+    ## Example:
+    ##  curl -X GET -d "passage=John3.16&key=c96a9a1e68c80d8d14d990ffada473da" http://api.biblia.com/v1/bible/content/LEB.js 
+    #def lookup reference, translation
+      #puts "http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da"
+      #HTTParty::get("http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da")
+    #end
+  #end
+#end
