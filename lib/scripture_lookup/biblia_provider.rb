6d74bed39521a22945212fb7c55b6593c94ab1be
Warren Wright <warren@thewrightangle.com>
2012-10-05 07:18:21 -0600

    Biblia work in progress

--- lib/scripture_lookup/biblia_provider.rb
+++ lib/scripture_lookup/biblia_provider.rb
@@ -1,4 +1,16 @@
+require 'httparty'
+
 module ScriptureLookup
   class BibliaProvider
+
+    ##
+    # Takes both a reference and a translation; returns a plain text translation.
+    #
+    # Example:
+    #  curl -X GET -d "passage=John3.16&key=c96a9a1e68c80d8d14d990ffada473da" http://api.biblia.com/v1/bible/content/LEB.js 
+    def lookup reference, translation
+      puts "http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da"
+      HTTParty::get("http://api.biblia.com/v1/bible/content/#{translation}.js?passage=#{URI.escape(reference)}&key=c96a9a1e68c80d8d14d990ffada473da")
+    end
   end
 end
