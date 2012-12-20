ec607109f730848784db93f7d9588eaa2f5728a7
Warren Wright <warren@thewrightangle.com>
2012-12-20 07:58:01 -0700

    Psalm 23 (ESV) passes!

--- lib/scripture_lookup/bible_gateway_scraper.rb
+++ lib/scripture_lookup/bible_gateway_scraper.rb
@@ -1,12 +1,18 @@
 require 'metainspector'
+require 'loofah'
 
 module ScriptureLookup
   class BibleGatewayScraper
     def lookup reference, version
       url = "http://www.biblegateway.com/passage/?search=#{reference}&version=#{version.to_s}"
       page = MetaInspector.new(url)
-      doc = page.parsed_document
-      doc.xpath('//div[contains(@class,"passage")]//p//span[contains(@class,"text")]').children.last.text
+      doc = Loofah.fragment(page.document)
+
+      kill_sup = Loofah::Scrubber.new { |node| node.remove if node.name == "sup" or node["class"] == "chapternum" }
+      doc.scrub!(kill_sup)
+
+      nodes = doc.xpath('.//div[contains(@class, "passage")]//p//span[contains(@class,"text")]')
+      nodes.to_a.join("\n")
     end
   end
 end
