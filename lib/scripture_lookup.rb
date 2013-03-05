a3ec9d4247f83a299bbfa9c36c1bd06f99edfc57
wrwright <warren@thewrightangle.com>
2013-03-05 09:26:46 -0700

    added default_provider accessor

--- lib/scripture_lookup.rb
+++ lib/scripture_lookup.rb
@@ -4,11 +4,19 @@ module ScriptureLookup
   require_relative 'scripture_lookup/errors/error'
   require_relative 'scripture_lookup/bible_gateway_scraper'
 
+  def default_provider
+    @default_provider ||= BibleGatewayScraper
+  end
+
+  def default_provider=(value)
+    @default_provider = value
+  end
+
   # Sugar method to be able to quickly create a provider.
   #
   # BibleGatewayScraper is the default provider as it can return scripture
   # from the most translations.
   def new
-    BibleGatewayScraper.new
+    self.default_provider.new
   end
 end
